34e66baf3e459dfcc52d11499a000c2e4ddbc825
Paul T <p.t.thanataweenont@gmail.com>
2021-08-28 16:30:57 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,19 @@
-// Function on menus
-// const navMenus = document.querySelectorAll(".navDefault");
-// const tabContent = document.querySelectorAll(".tabContent");
-
-// for (i = 1; i < tabContent.length; i++) {
-//     tabContent[i].hidden = true;
-// }
-
-// for (let tab of navMenus) {
-//     tab.addEventListener("click", tabNameActive);
-// }
-
-// function tabNameActive() {
-//     const tabActive = document.querySelectorAll(".tabNameActive");
-//     const dataTab = this.getAttribute("data-tab");
-//     for (let tab of tabActive) {
-//         tab.classList.value = "tabName";
-//         for (let content of tabContent) {
-//             if (content.dataset.tab !== dataTab) {
-//                 content.hidden = true;
-//             } else {
-//                 content.hidden = false;
-//             }
-//         }
-//     }
-//     this.classList.value = "tabNameActive";
-// }
-
-
-// Remove the transition class
-const square = document.querySelector('.projectPanel');
-square.classList.remove('projectPanel-transition');
-
-// Create the observer, same as before:
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      square.classList.add('projectPanel-transition');
-      return;
-    }
-
-    square.classList.remove('projectPanel-transition');
-  });
-});
-
-observer.observe(document.querySelector('.projectPanel-wrapper'));
+const projectPanel = document.querySelectorAll('.projectPanel');
+const options = {
+rootMargin: '0px',
+threshold: 0.50
+}
+const callback = (entries) => {
+  entries.forEach((entry) => {
+    const target = entry.target;
+    if (entry.intersectionRatio >= 0.50) {
+    target.classList.add("projectPanel-transition");
+    } else {
+    target.classList.remove("projectPanel-transition");
+   }
+  })
+}
+const observer = new IntersectionObserver(callback, options)
+projectPanel.forEach((section, index) => {
+observer.observe(section)
+})
