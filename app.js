82256cdf2119986fcfd3ef3205a73c69250e6f29
Paul T <p.t.thanataweenont@gmail.com>
2021-09-04 00:20:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,44 +4,45 @@
   const projectPanel = document.querySelectorAll('.projectPanel');
   const options = {
   rootMargin: '0px',
-  threshold: 0.45
+  threshold: 0.50
   };
-  const callbackRight = (entries) => {
+    const callbackLeft = (entries) => {
     entries.forEach((entry) => {
       const target = entry.target;
-      if (entry.intersectionRatio >= 0.45) {
-      target.classList.add("aboutMeRight-transition");
-      } else {
-      target.classList.remove("aboutMeRight-transition");
-     }
-    })
-  };
-  const callbackLeft = (entries) => {
-    entries.forEach((entry) => {
-      const target = entry.target;
-      if (entry.intersectionRatio >= 0.45) {
+      if (entry.intersectionRatio >= 0.50) {
       target.classList.add("aboutMeLeft-transition");
       } else {
       target.classList.remove("aboutMeLeft-transition");
      }
     })
   };
+  const callbackRight = (entries) => {
+    entries.forEach((entry) => {
+      const target = entry.target;
+      if (entry.intersectionRatio >= 0.50) {
+      target.classList.add("aboutMeRight-transition");
+      } else {
+      target.classList.remove("aboutMeRight-transition");
+     }
+    })
+  };
+
   const callbackProject = (entries) => {
     entries.forEach((entry) => {
       const target = entry.target;
-      if (entry.intersectionRatio >= 0.45) {
+      if (entry.intersectionRatio >= 0.50) {
       target.classList.add("projectPanel-transition");
       } else {
       target.classList.remove("projectPanel-transition");
      }
     })
   }
-  const observerRight = new IntersectionObserver(callbackRight, options);
-  aboutMeRight.forEach((section, index) => {
-  observerRight.observe(section);
   const observerLeft = new IntersectionObserver(callbackLeft, options);
   aboutMeLeft.forEach((section, index) => {
   observerLeft.observe(section);
+  const observerRight = new IntersectionObserver(callbackRight, options);
+  aboutMeRight.forEach((section, index) => {
+  observerRight.observe(section);
   const observerProject = new IntersectionObserver(callbackProject, options)
   projectPanel.forEach((section, index) => {
   observerProject.observe(section)
@@ -50,4 +51,3 @@
 
   slideShow()
 
-  
