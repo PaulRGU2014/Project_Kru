0e48ac90e65c89809377033bb01e57b716cbc3db
Paul T <p.t.thanataweenont@gmail.com>
2021-09-04 22:26:51 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,108 +1,29 @@
 // For all slideshows
- function slideShow () { const aboutMeRight = document.querySelectorAll('.aboutMeRight');
-  const aboutMeLeft = document.querySelectorAll('.aboutMeLeft');
-  const projectPanel = document.querySelectorAll('.projectPanel');
-  const timelineStone = document.querySelectorAll('.timelineStone');
-  const timelineYear = document.querySelectorAll('.timelineYear');
-  const timelineContent = document.querySelectorAll('.timelineContent');
+ function slideShow () { 
+  const transition = document.querySelectorAll('#transition');
 
   const options = {
   rootMargin: '0px 0px',
   threshold: 0.5
   };
   // About Me Section
-    const callbackLeft = (entries) => {
+    const callbackTransition = (entries) => {
     entries.forEach((entry) => {
       const target = entry.target;
       if (entry.intersectionRatio >= 0.5) {
-      target.classList.add("aboutMeLeft-transition");
+      target.classList.add("transition");
       } else {
-      target.classList.remove("aboutMeLeft-transition");
+      target.classList.remove("transition");
      }
     })
   };
-  const callbackRight = (entries) => {
-    entries.forEach((entry) => {
-      const target = entry.target;
-      if (entry.intersectionRatio >= 0.5) {
-      target.classList.add("aboutMeRight-transition");
-      } else {
-      target.classList.remove("aboutMeRight-transition");
-     }
-    })
-  };
-// Timelines:Stone
-  const callbackStone = (entries) => {
-    entries.forEach((entry) => {
-      const target = entry.target;
-      if (entry.intersectionRatio >= 0.5) {
-      target.classList.add("timelineStone-transition");
-      } else {
-      target.classList.remove("timelineStone-transition");
-     }
-    })
-  };
-// Timelines:Year
-const callbackYear = (entries) => {
-  entries.forEach((entry) => {
-    const target = entry.target;
-    if (entry.intersectionRatio >= 0.5) {
-    target.classList.add("timelineYear-transition");
-    } else {
-    target.classList.remove("timelineYear-transition");
-   }
-  })
-};
-
-// Timeline:Content
-const callbackTimelineContent = (entries) => {
-  entries.forEach((entry) => {
-    const target = entry.target;
-    if (entry.intersectionRatio >= 0.5) {
-    target.classList.add("timelineContent-transition");
-    } else {
-    target.classList.remove("timelineContent-transition");
-   }
-  })
-};
-
-// Projects Section
-  const callbackProject = (entries) => {
-    entries.forEach((entry) => {
-      const target = entry.target;
-      if (entry.intersectionRatio >= 0.5) {
-      target.classList.add("projectPanel-transition");
-      } else {
-      target.classList.remove("projectPanel-transition");
-     }
-    })
-  };
+  
   // Execution Part
-  const observerLeft = new IntersectionObserver(callbackLeft, options);
-  aboutMeLeft.forEach((section, index) => {
+  const observerLeft = new IntersectionObserver(callbackTransition, options);
+  transition.forEach((section, index) => {
   observerLeft.observe(section);
 
-  const observerRight = new IntersectionObserver(callbackRight, options);
-  aboutMeRight.forEach((section, index) => {
-  observerRight.observe(section);
-
-  const observerProject = new IntersectionObserver(callbackProject, options)
-  projectPanel.forEach((section, index) => {
-  observerProject.observe(section);
-
-  const observerStone = new IntersectionObserver(callbackStone, options)
-  timelineStone.forEach((section, index) => {
-  observerStone.observe(section);
-
-  const observerYear = new IntersectionObserver(callbackYear, options)
-  timelineYear.forEach((section, index) => {
-  observerYear.observe(section);
-
-  const observerTimelineContent = new IntersectionObserver(callbackTimelineContent, options)
-  timelineContent.forEach((section, index) => {
-  observerTimelineContent.observe(section);
-
-  })})})})})})}
+  })}
 
   slideShow()
 
