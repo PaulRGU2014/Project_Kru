09bfdaa8ef1334502f1a0472a0684298e635d162
Paul T <p.t.thanataweenont@gmail.com>
2021-09-04 19:48:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,15 @@
  function slideShow () { const aboutMeRight = document.querySelectorAll('.aboutMeRight');
   const aboutMeLeft = document.querySelectorAll('.aboutMeLeft');
   const projectPanel = document.querySelectorAll('.projectPanel');
+  const timelineStone = document.querySelectorAll('.timelineStone');
+  const timelineYear = document.querySelectorAll('.timelineYear');
+  const timelineContent = document.querySelectorAll('.timelineContent');
+
   const options = {
   rootMargin: '0px 0px',
   threshold: 0.5
   };
+  // About Me Section
     const callbackLeft = (entries) => {
     entries.forEach((entry) => {
       const target = entry.target;
@@ -26,7 +31,42 @@
      }
     })
   };
+// Timelines:Stone
+  const callbackStone = (entries) => {
+    entries.forEach((entry) => {
+      const target = entry.target;
+      if (entry.intersectionRatio >= 0.5) {
+      target.classList.add("timelineStone-transition");
+      } else {
+      target.classList.remove("timelineStone-transition");
+     }
+    })
+  };
+// Timelines:Year
+const callbackYear = (entries) => {
+  entries.forEach((entry) => {
+    const target = entry.target;
+    if (entry.intersectionRatio >= 0.5) {
+    target.classList.add("timelineYear-transition");
+    } else {
+    target.classList.remove("timelineYear-transition");
+   }
+  })
+};
 
+// Timeline:Content
+const callbackTimelineContent = (entries) => {
+  entries.forEach((entry) => {
+    const target = entry.target;
+    if (entry.intersectionRatio >= 0.5) {
+    target.classList.add("timelineContent-transition");
+    } else {
+    target.classList.remove("timelineContent-transition");
+   }
+  })
+};
+
+// Projects Section
   const callbackProject = (entries) => {
     entries.forEach((entry) => {
       const target = entry.target;
@@ -36,18 +76,33 @@
       target.classList.remove("projectPanel-transition");
      }
     })
-  }
+  };
+  // Execution Part
   const observerLeft = new IntersectionObserver(callbackLeft, options);
   aboutMeLeft.forEach((section, index) => {
   observerLeft.observe(section);
+
   const observerRight = new IntersectionObserver(callbackRight, options);
   aboutMeRight.forEach((section, index) => {
   observerRight.observe(section);
+
   const observerProject = new IntersectionObserver(callbackProject, options)
   projectPanel.forEach((section, index) => {
-  observerProject.observe(section)
+  observerProject.observe(section);
 
-  })})})}
+  const observerStone = new IntersectionObserver(callbackStone, options)
+  timelineStone.forEach((section, index) => {
+  observerStone.observe(section);
+
+  const observerYear = new IntersectionObserver(callbackYear, options)
+  timelineYear.forEach((section, index) => {
+  observerYear.observe(section);
+
+  const observerTimelineContent = new IntersectionObserver(callbackTimelineContent, options)
+  timelineContent.forEach((section, index) => {
+  observerTimelineContent.observe(section);
+
+  })})})})})})}
 
   slideShow()
 
