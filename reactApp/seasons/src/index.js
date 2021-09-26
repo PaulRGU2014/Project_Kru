d512c1850c8fbe2e18f043d0931ff43dca14613a
Paul T <p.t.thanataweenont@gmail.com>
2021-09-26 17:11:41 -0600

    Update index.js

--- reactApp/seasons/src/index.js
+++ reactApp/seasons/src/index.js
@@ -4,6 +4,15 @@ import ReactDOM from "react-dom";
 // when we use class, we create a class with 1 method that's assigned to it
 
 class App extends React.Component {
+    // We have to call a constructor function before anything else!! -- initialize state
+    constructor(props) {
+        super(props)
+        // we use this to make sure that the React.component get called and we have to do it everytime
+
+        this.state = { lat: null };
+    }
+
+    // React says we have tp define render!!
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
