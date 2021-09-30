4bf7d75709325c383940b7c1164f7810bb1e2275
Paul T <p.t.thanataweenont@gmail.com>
2021-09-29 19:10:00 -0600

    Update index.js

--- reactApp/seasons/src/index.js
+++ reactApp/seasons/src/index.js
@@ -1,24 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 // when we use class, we create a class with 1 method that's assigned to it
 
 class App extends React.Component {
-    // We have to call a constructor function before anything else!! -- initialize state
-    constructor(props) {
-        super(props)
-        // we use this to make sure that the React.component get called and we have to do it everytime
+    // // We have to call a constructor function before anything else!! -- initialize state
+    // constructor(props) {
+    //     super(props)
+    //     // we use this to make sure that the React.component get called and we have to do it everytime
 
-        // THIS IS THE ONLY TIME we do direct assignment to this.state
+    //     // THIS IS THE ONLY TIME we do direct assignment to this.state
 
-        this.state = { lat: null, errorMessage: '' };
-        // We call for a current location here in order not to double fetch
+    //     this.state = { lat: null, errorMessage: '' };
+    //     // We call for a current location here in order not to double fetch
+
+    // }
+
+    // We can shorten it by:
+
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            //  We called setstate!!!
-            position => { this.setState({ lat: position.coords.latitude }); },
-            err => {
-                this.setState({ errorMessage: err.message })
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
 
@@ -28,10 +35,10 @@ class App extends React.Component {
             return <div>Error: {this.state.errorMessage}</div>
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Lattitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading...</div>
+        return <div><Spinner /></div>
     }
 }
 
