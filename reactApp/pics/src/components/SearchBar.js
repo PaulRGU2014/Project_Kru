a4d85c0a6843457b72fd90204029a051c43432c6
Paul T <p.t.thanataweenont@gmail.com>
2021-09-30 22:46:15 -0600

    Update SearchBar.js

--- reactApp/pics/src/components/SearchBar.js
+++ reactApp/pics/src/components/SearchBar.js
@@ -2,12 +2,27 @@ import React from "react";
 
 // In this case, we want a class based component
 class SearchBar extends React.Component {
+    onInputChange(event) {
+        console.log(event.target.value)
+    }
+
+    onInputClick() {
+        console.log('input was clicked')
+    }
+
+
     render() {
-        return (<div>
-            <form>
-                <input type='text' />
+        return (<div className='ui segment'>
+            <form className='ui form'>
+                <div className='field'>
+                    <label>Image Search</label>
+                    <input type='text' onClick={this.onInputClick} onChange={this.onInputChange} />
+                    {/* we don't put 'onInputChange()' because 
+                    we don't want to call when the component is rendered. It's a callback bunction*/}
+                </div>
             </form>
-        </div>);
+        </div>
+        );
     }
 }
 
