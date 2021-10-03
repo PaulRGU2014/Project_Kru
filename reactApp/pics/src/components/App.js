f6f9aba3b2dd84272f4c88bad6c7ba8d330b3b84
Paul T <p.t.thanataweenont@gmail.com>
2021-10-03 13:46:09 -0600

    Update App.js

--- reactApp/pics/src/components/App.js
+++ reactApp/pics/src/components/App.js
@@ -1,15 +1,28 @@
 import React from "react";
+import unsplash from "../api/unsplash";
 import SearchBar from './SearchBar';
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term);
+    // we start from set a state
+
+    state = { images: [] }
+
+    // Anything that takes time, we put async in front of the method
+    onSearchSubmit = async (term) => {
+        // get a request from unsplash and put await for the return
+        const response = await unsplash.get('/search/photos', {
+            params: { query: term },
+
+        });
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
             </div>
         );
     }
