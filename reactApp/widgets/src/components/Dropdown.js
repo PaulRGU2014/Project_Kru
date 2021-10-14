2e2c73e6b77efe1e6c23d5ec3762996f4c24fb99
Paul T <p.t.thanataweenont@gmail.com>
2021-10-14 17:50:25 -0600

    Create Dropdown.js

---
+++ reactApp/widgets/src/components/Dropdown.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        document.body.addEventListener('click', (event) => {
+            // console.log(event.target);
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+
+            setOpen(false);
+        }, { capture: true })
+    });
+
+    const renderedOptons = options.map((option) => {
+        if (option.value === selected.value) {
+            return null;
+        }
+
+        return (
+            <div
+                key={option.value}
+                className="item"
+                onClick={() => {
+                    // console.log('Item Clicked');
+                    onSelectedChange(option)
+                }}
+            >
+
+                {option.label}
+            </div>
+        );
+    });
+
+    // console.log(ref.current);
+
+    return (
+        <div ref={ref} className="ui form">
+            <div className="field">
+                <label className="label">Select a Color</label>
+                <div onClick={() => {
+                    // console.log('Dropdown Clicked')
+                    setOpen(!open)
+                }}
+                    // This is how to toggle the className
+                    className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+                >
+                    <i className="dropdown icon"></i>
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
+                        {renderedOptons}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Dropdown;
