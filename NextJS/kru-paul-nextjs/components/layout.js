f7b47e8ef0e23e213754ea33d011a02050a24483
Paul Thanataweenont <paul.thanataweenont@outlook.com>
2022-01-02 14:48:39 -0700

    commit

--- NextJS/kru-paul-nextjs/components/layout.js
+++ NextJS/kru-paul-nextjs/components/layout.js
@@ -1,6 +1,7 @@
 import Header from "./Header/Header"
 import Menus from "./Menus/Menus"
 import Footer from "./Footer/Footer"
+import '../styles/global-style.scss'
 
 export default function Layout({ children }) {
     return (
--- NextJS/kru-paul-nextjs/pages/index.js
+++ NextJS/kru-paul-nextjs/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import '../styles/global-style.scss'
 import Header from "../components/Header/Header"
 import Menus from "../components/Menus/Menus"
 import AboutMe from "../components/AboutMe/AboutMe"
