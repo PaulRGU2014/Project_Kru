6fdb151a61e9990b4bee3bf09afa76e81080dc23
Paul Thanataweenont <paul.thanataweenont@outlook.com>
2022-01-04 21:54:37 -0700

    commit

--- NextJS/kru-paul-nextjs/.next/static/chunks/webpack.js
+++ NextJS/kru-paul-nextjs/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "83ce7330a7ee0831"; }
+/******/ 		__webpack_require__.h = function() { return "94dd604888629a0d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- NextJS/kru-paul-nextjs/pages/index.js
+++ NextJS/kru-paul-nextjs/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import Head from 'next/head'
+import PaulChemLogo from '../components/pics/logo/Logo_small.png'
 
 import Header from "../components/Header/Header"
 import Menus from "../components/Menus/Menus"
@@ -14,11 +15,13 @@ function HomePage() {
     return (
         <div>
             <Head>
+                <title>Paul Thanataweenont's official website</title>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Neucha&display=swap" rel="stylesheet" />
+                <link rel="icon" href="../components/pics/logo/Logo_small.png"></link>
             </Head>
-        </div>
+        </div >
     )
 }
 
